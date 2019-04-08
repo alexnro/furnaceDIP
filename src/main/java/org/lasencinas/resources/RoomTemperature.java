6ab9f34d727097a4fb1026a069be0824c1d857cc
alexnro <alexjandron99@gmail.com>
2019-04-08 21:36:52 +0200

    Creados constructor y metodos

--- src/main/java/org/lasencinas/resources/RoomTemperature.java
+++ src/main/java/org/lasencinas/resources/RoomTemperature.java
@@ -1,4 +1,18 @@
 package org.lasencinas.resources;
 
 public class RoomTemperature {
+
+    private double temperature = 0;
+
+    public RoomTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getTemperature() {
+        return this.temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
 }
