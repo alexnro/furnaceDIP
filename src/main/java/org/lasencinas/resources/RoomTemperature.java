7f0f584449ddbd398ce8b431c3d7f98f9e602f10
alexnro <alexjandron99@gmail.com>
2019-04-08 22:13:06 +0200

    Añadido singleton

--- src/main/java/org/lasencinas/resources/RoomTemperature.java
+++ src/main/java/org/lasencinas/resources/RoomTemperature.java
@@ -3,6 +3,18 @@ package org.lasencinas.resources;
 public class RoomTemperature {
 
     private double temperature = 0;
+    private static RoomTemperature INSTANCE = null;
+
+    public RoomTemperature(){
+
+    }
+
+    public static RoomTemperature getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new RoomTemperature();
+        }
+        return INSTANCE;
+    }
 
     public RoomTemperature(double temperature) {
         this.temperature = temperature;
